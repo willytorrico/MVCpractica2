a64a30b5d7ae0498abc6a2fd23ed50d67d20bc28
Willy Torrico <anymelwi@gmail.com>
2024-10-27 23:42:40 -0400

    Se agrego mi proyecto

---
+++ MVCpractica2/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCpractica2.Models;
+
+namespace MVCpractica2.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        //Aqui todos los modelos que se creen 
+        //public DbSet<Producto> Producto { get; set; }
+        public DbSet<Producto> Producto { get; set; }
+    }
+
+
+
+}
---
+++ MVCpractica2/Models/Producto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCpractica2.Models
+{
+    public class Producto
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [Display(Name = "Nombre del Producto")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "La descripción es obligatoria")]
+        public string Descripcion { get; set; }
+    }
+
+}
